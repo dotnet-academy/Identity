eaf6077c5e06b7af3ad2eaff713ad973c0170aea
Konstantinos Athanasoglou <kathanasoglou@vivawallet.com>
2018-06-30 01:02:41 +0300

    remove unused view model

--- src/CrowdFunding/CrowdFunding/Models/PersonViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CrowdFunding.Models
-{
-    public partial class PersonViewModel
-    {
-        public string Firstname { get; set; }
-        public string Lastname { get; set; }
-        public string ProfileUrl { get; set; }
-    }
-}
